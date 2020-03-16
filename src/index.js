fc6bfa15681b10f9823efc569076f5c863578cc1
Irving Didier <bider88@gmail.com>
2020-03-15 18:21:52 -0600

    using components through classes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './styles.scss'
 
-// const world = 'world'
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// REACT CORE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// const user = {
-//   name: 'Pepe',
-//   age: 24,
-//   gender: 'male',
-//   country: 'MEX'
-// }
+/*
+const world = 'world'
 
-// const nationality = user => `I'm from ${user.country}`
+const user = {
+  name: 'Pepe',
+  age: 24,
+  gender: 'male',
+  country: 'MEX'
+}
 
-// const App = <h1>Hello {world} from react</h1>
-// const User = <p>My name is {user.name} and I'm {user.age} old.</p>
-// const Info = <p>{nationality(user)}</p>
+const nationality = user => `I'm from ${user.country}`
 
-// const Complete = React.createElement('div', null, App, User, Info)
+const App = <h1>Hello {world} from react</h1>
+const User = <p>My name is {user.name} and I'm {user.age} old.</p>
+const Info = <p>{nationality(user)}</p>
 
-// ReactDom.render(Complete, document.getElementById('root'))
+const Complete = React.createElement('div', null, App, User, Info)
+
+ReactDom.render(Complete, document.getElementById('root'))
+*/
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// USING COMPONENTS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-
+/*
 const SimpleCardFruit = () => (
   <div>
     <h3>title</h3>
@@ -73,7 +80,71 @@ const App = () => (
   </div>
 )
 
-// const Complete = React.createElement('div', null, <App/>)
-
-// ReactDom.render(Complete, document.getElementById('root'))
 ReactDom.render(<App/>, document.getElementById('root'))
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// COMPONENTS USING CLASSES
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CardFruit extends React.Component {
+  
+  constructor() {
+    super()
+
+    // this.add = this.add.bind(this)
+    // this.remove = this.remove.bind(this)
+
+    const METHODS = [
+      'add',
+      'remove',
+      'clear'
+    ]
+
+    METHODS.forEach(method => this[method] = this[method].bind(this))
+
+    this.state = {
+      quantity: 0
+    }
+  }
+
+  add() {
+    this.setState({ quantity: this.state.quantity + 1 })
+  }
+  
+  remove() {
+    this.setState({ quantity: this.state.quantity - 1 })
+  }
+  
+  clear() {
+    this.setState({ quantity: 0 })
+  }
+
+  render() {
+      return (
+        <div>
+          <h3>{this.props.name}</h3>
+          <p><small>Quantity: { this.state.quantity }</small></p>
+          <button className="button is-link" onClick={ this.add } title="add"> + </button>
+          <button className="button is-warning" onClick={ this.remove } title="remove"> - </button>
+          <button className="button is-primary" onClick={ this.clear } title="clear"> x </button>
+          <p>$ {this.props.price}</p>
+          <hr/>
+        </div>
+      )
+  }
+}
+
+
+const App = () => (
+  <div>
+    <CardFruit name={'Cocoa'} price={12.3} />
+    <CardFruit name='Strawberry' price={7.9} />
+    <CardFruit name='Orange' price={10.1} />
+    <br/>
+    <button className="button is-danger is-outlined">Hello</button>
+  </div>
+)
+
+ReactDom.render(<App/>, document.getElementById('root'))
+
