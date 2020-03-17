e07134fd5c7483145ddf257480a767596675ce4f
Irving Didier <bider88@gmail.com>
2020-03-17 15:21:02 -0600

    adding inline styles to components

--- src/components/card/CardFruit.js
+++ src/components/card/CardFruit.js
@@ -13,14 +13,26 @@ class CardFruit extends React.Component {
   clear = () => this.setState({ quantity: 0 })
 
   render() {
+
+    const hasItems = this.state.quantity > 0 ? '#44fc5f' : this.state.quantity < 0 ? '#ff6d19' : '#c9c9c9'
+
+    const styles = {
+      border: '2px solid',
+      borderColor: hasItems,
+      padding: '1em',
+      marginBottom: '5px',
+      borderRadius: '5px',
+      transition: 'all 400ms ease-out'
+    }
       return (
-        <div>
+        <div style={ styles }>
           <h3>{this.props.name}</h3>
           <p><small>Quantity: { this.state.quantity }</small></p>
           <button className="button is-link" onClick={ this.add } title="add"> + </button>
           <button className="button is-warning" onClick={ this.remove } title="remove"> - </button>
           <button className="button is-primary" onClick={ this.clear } title="clear"> x </button>
           <p>$ {this.props.price}</p>
+          <p><b>Total: </b> ${ (this.state.quantity * this.props.price).toFixed(2) }</p>
           <hr/>
         </div>
       )
